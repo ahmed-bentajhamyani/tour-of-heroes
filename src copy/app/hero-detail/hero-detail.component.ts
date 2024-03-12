a8ee69fcd830d5fb25609326427051f188c7dc08
Ahmed Bentaj Hamyani <abentajhamyani@smb.sqli.com>
2024-03-12 17:21:15 +0000

    initial commit

---
+++ src copy/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
+import { Location } from '@angular/common';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent {
+  hero: Hero = {
+    id: 0,
+    name: ''
+  };
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack() {
+    this.location.back();
+  }
+}
